9d443c14133d040cd34e9b380681b609862612e1
Gabby Ortman <gabrielle.ortman@cerner.com>
2017-06-26 12:58:56 -0500

    refactor day zero: weighted mean

--- zero/weighted_mean.py
+++ zero/weighted_mean.py
@@ -7,35 +7,20 @@
 	Your answer should be rounded to a scale of  decimal place (i.e.,  format).
 
 	Gabby Ortman 
-	30 December 2016
+	26 June 2017
 """
 
-class WeightedMean(object):
-	"""
-		Class representing the Weighted Mean 
-	"""
-	def __init__(self, arr, num_elements, weight_arr):
-		self.arr = arr 
-		self.num_elements = num_elements
-		self.weight_arr = weight_arr 
-		self.weighted_mean = None
+def calculate_weighted_mean(arr, weight_arr, num_elements): 
+	numerator = 0 
+	denominator = sum(weight_arr)
+	for i in range(num_elements): 
+		numerator += arr[i] * weight_arr[i]
 
-	def calculate_weighted_mean(self): 
-		numerator = 0 
-		denominator = sum(self.weight_arr)
-		for i in range(self.num_elements): 
-			numerator += self.arr[i] * self.weight_arr[i]
-
-		self.weighted_mean = numerator / denominator
-
-	def get_wmean(self): 
-		return self.weighted_mean
+	return numerator / denominator
 		
 if __name__ == '__main__':
 	num_elements = int(input().strip())
 	arr = [int(x) for x in input().split()]
 	weight_arr = [int(x) for x in input().split()]
-	GabbysAmazingStatsMachine = WeightedMean(arr, num_elements, weight_arr)
-	GabbysAmazingStatsMachine.calculate_weighted_mean()
-	wmean = GabbysAmazingStatsMachine.get_wmean() 
+	wmean = calculate_weighted_mean(arr, weight_arr, num_elements)
 	print("{0:.1f}".format(wmean))
